b8e2bad8fb647380c3d4c5a001c401a006c8bf4d
Tyler Johnson <tyler@csdconsultants.us>
2021-07-30 20:41:48 -0700

    created auth.js component

---
+++ src/components/todo/Auth.js
@@ -0,0 +1,17 @@
+import { useContext } from 'react';
+import { AuthContext } from '../../context/Auth.js';
+
+function Auth (props) {
+  const { isAuthenticated, isAuthorized } = useContext(AuthContext);
+
+  return (
+    <>
+      {(isAuthenticated && isAuthorized(props.capability))
+        ? props.children
+        : null
+        }
+    </>
+  )
+}
+
+export default Auth;
